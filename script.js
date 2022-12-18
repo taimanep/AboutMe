8f037dcf5bb1c273a864da04387bb41e4f1a348f
taimanep <120290557+taimanep@users.noreply.github.com>
2022-12-17 18:26:49 -1000

    Add files via upload
    Added JS file

---
+++ script.js
@@ -0,0 +1,13 @@
+function before() {
+    document.getElementById('myImage')
+    .src="/Users/westsidepainting/Desktop/PORTFOLIO/AboutMe/resources/css/images/img2.jpeg";
+    document.getElementById('message')
+    .innerHTML="Hi code programmer";
+}
+
+function afterr() {
+    document.getElementById('myImage')
+    .src="//Users/westsidepainting/Desktop/PORTFOLIO/AboutMe/resources/css/images/img1.jpeg";
+    document.getElementById('message')
+    .innerHTML="Bye code programmer";
+}
